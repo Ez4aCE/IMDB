2c646bf2254da182a6902ff1e2f61471d1782d3f
Ez4aCE <himanshu24rai@gmail.com>
2024-07-21 14:13:02 +0530

    chaning theme color settings

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Providers({ children }) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <div className="min-h-screen dark:bg-gray-700 dark:text-gray-200 light:text-gray-700 transition-colors duration-500">
+      <div className="bg-emerald-50 min-h-screen dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-500">
         {children}
       </div>
     </ThemeProvider>
