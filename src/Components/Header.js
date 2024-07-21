036800a9f3cd0a22c833f2c495fbfbb1814fa2f5
Ez4aCE <himanshu24rai@gmail.com>
2024-07-21 14:00:26 +0530

    Added Darkmode

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,19 +3,21 @@ import MenuItem from "./MenuItem";
 import { TiHome } from "react-icons/ti";
 import { IoMdInformationCircle } from "react-icons/io";
 import Link from "next/link";
+import DarkModeSwitch from "./DarkModeSwitch";
 
 export default function Header() {
   return (
     <div className="flex justify-between mx-2 max-w-6x1  sm:mx-auto items-center py-3">
-      <div className="flex">
+      <div className="flex ml-10 select-none">
         <MenuItem title="HOME" address="/" Icon={TiHome} />
         <MenuItem title="ABOUT" address="/about" Icon={IoMdInformationCircle} />
       </div>
 
-      <div className="flex items-center space-x-5">
+      <div className="flex items-center space-x-5 mr-10">
+        <DarkModeSwitch />
         <Link href="/">
-          <h2 className="text-2xl">
-            <span className="font-bold bg-amber-500 rounded-lg py-1 px-2 mr-1">
+          <h2 className="text-2xl cursor-text select-none">
+            <span className="font-bold bg-amber-500 rounded-lg py-1 px-2 mr-1 v">
               IMDB
             </span>
             <span className="text-xl hidden sm:inline mr-2">CLONE</span>
